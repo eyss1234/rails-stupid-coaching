9b84eb33c05288fc9d1b2e40f7d95610dfffb3a4
Emily Santos <eyssantos@gmail.com>
2022-07-13 10:01:35 +0100

    coach created

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,17 @@
+class QuestionsController < ApplicationController
+    def ask
+    end
+
+    def answer
+      if params[:question] == "I am going to work"
+        @test = "Great!"
+      elsif params[:question].include? "?"
+        @test = "Silly question, get dressed and go to work!"
+      else
+        @test = " I don't care, get dressed and go to work!"
+      end  
+    #   @test = params
+    end
+end
+
+    
