b047e8b148e913a00a0792419b0dda621aaa62f9
Emily Santos <eyssantos@gmail.com>
2022-07-14 19:57:10 +0100

    bootstrap styling added

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -12,6 +12,7 @@ class QuestionsController < ApplicationController
       end  
     #   @test = params
     end
+
 end
 
     
--- test/system/questions_test.rb
+++ test/system/questions_test.rb
@@ -2,8 +2,13 @@
 require "application_system_test_case"
 
 class QuestionsTest < ApplicationSystemTestCase
-  test "visiting /ask renders the form" do
+  # [...]
+
+  test "saying Hello yields a grumpy response from the coach" do
     visit ask_url
-    assert_selector "p", text: "Ask your coach anything"
+    fill_in "question", with: "Hello"
+    click_on "Ask"
+
+    assert_text "I don't care, get dressed and go to work!"
   end
 end
